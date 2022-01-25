12556ad99f43e5ffe41d76f7b446f9ec9aa793c0
minjun8946 <minjun8946@daum.net>
2022-01-25 16:17:33 +0900

    ⚡️:: flow

--- SimpleArchitecture1/data/src/main/java/com/example/data/remote/repository/RepositoryImpl.kt
+++ SimpleArchitecture1/data/src/main/java/com/example/data/remote/repository/RepositoryImpl.kt
@@ -7,14 +7,14 @@ import com.example.domain.data.Team
 import com.example.domain.repository.Repository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class RepositoryImpl @Inject constructor(
     private val dataSource : DataSource
 ) : Repository {
-    override suspend fun getTeamData(): Flow<List<Team>> =
-        OfflineCacheUtil<List<Team>>()
-            .remoteData { dataSource.getTeamData().map { it.toTeam() } }
-            .createFlow()
+    override suspend fun getTeamData(): Flow<List<Team>> = flow {
+        dataSource.getTeamData()
+    }
 
 }
