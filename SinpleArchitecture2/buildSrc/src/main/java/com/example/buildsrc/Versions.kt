485c10bcfda1cdb8e5172f4b6b7b0e912c6c5f15
minjun8946 <minjun8946@daum.net>
2022-01-28 23:36:23 +0900

    ⚙️:: versions

---
+++ SinpleArchitecture2/buildSrc/src/main/java/com/example/buildsrc/Versions.kt
@@ -0,0 +1,26 @@
+package com.example.buildsrc
+
+object Versions {
+
+    /* build tools */
+    const val buildTools = "31.0.0"
+
+    /* plugins */
+    const val androidPlugin = "7.1.0"
+    const val googleServices = "4.3.10"
+
+    /* libraries */
+    const val compose = "1.0.1"
+    const val composeConstraint = "1.0.0"
+    const val navigation = "2.4.0-rc01"
+    const val core = "1.6.0"
+    const val activity = "1.4.0"
+    const val appCompat = "1.3.1"
+    const val lifecycle = "2.3.1"
+    const val hilt = "2.38.1"
+    const val hiltCompose = "1.0.0-rc01"
+    const val coroutines = "1.6.0"
+    const val retrofit = "2.9.0"
+    const val okhttp = "5.0.0-alpha.3"
+    const val test = "1.4.1-alpha03"
+}
