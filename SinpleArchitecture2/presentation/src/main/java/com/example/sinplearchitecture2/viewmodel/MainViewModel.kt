6e48535f5ad9a348e87ecda8fc7f90add21967e5
minjun8946 <minjun8946@daum.net>
2022-01-29 23:41:57 +0900

    ⚡️:: hilt viewmodel

--- SinpleArchitecture2/presentation/src/main/java/com/example/sinplearchitecture2/viewmodel/MainViewModel.kt
+++ SinpleArchitecture2/presentation/src/main/java/com/example/sinplearchitecture2/viewmodel/MainViewModel.kt
@@ -2,7 +2,14 @@ package com.example.sinplearchitecture2.viewmodel
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
 
-class MainViewModel(
-    val savedStateHandle: SavedStateHandle
-) : ViewModel()
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val handle: SavedStateHandle,
+) : ViewModel(){
+    
+}
