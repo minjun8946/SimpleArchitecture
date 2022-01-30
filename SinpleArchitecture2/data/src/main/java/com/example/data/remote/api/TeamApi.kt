3a03353799949188322925380d1ee5e797beb140
minjun8946 <minjun8946@daum.net>
2022-01-30 23:36:40 +0900

    ⚡️:: team api

---
+++ SinpleArchitecture2/data/src/main/java/com/example/data/remote/api/TeamApi.kt
@@ -0,0 +1,10 @@
+package com.example.data.remote.api
+
+import com.example.data.remote.dto.TeamResponse
+import retrofit2.http.GET
+
+interface TeamApi {
+
+    @GET("/team")
+    suspend fun getTeam(): TeamResponse
+}
