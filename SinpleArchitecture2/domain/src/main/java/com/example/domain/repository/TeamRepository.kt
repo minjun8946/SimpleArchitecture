f7c48f3f875267d43e2c43643f11aaa8327efe17
minjun8946 <minjun8946@daum.net>
2022-01-31 23:46:49 +0900

    📑:: repositoryimpl

---
+++ SinpleArchitecture2/domain/src/main/java/com/example/domain/repository/TeamRepository.kt
@@ -0,0 +1,10 @@
+package com.example.domain.repository
+
+import com.example.domain.entity.TeamEntity
+import kotlinx.coroutines.flow.Flow
+
+interface TeamRepository {
+
+    suspend fun getTeam(): Flow<TeamEntity>
+
+}
