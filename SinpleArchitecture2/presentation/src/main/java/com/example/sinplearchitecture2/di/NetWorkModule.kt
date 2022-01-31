ff00759fe0463a8421ad70a3e6632c720caeb058
minjun8946 <minjun8946@daum.net>
2022-01-31 23:49:10 +0900

    ⚡️:: network module

---
+++ SinpleArchitecture2/presentation/src/main/java/com/example/sinplearchitecture2/di/NetWorkModule.kt
@@ -0,0 +1,47 @@
+package com.example.sinplearchitecture2.di
+
+import com.example.data.remote.api.TeamApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetWorkModule {
+    private const val BASE_URL = "https://www.balldontlie.io/api/v1/"
+
+    @Singleton
+    @Provides
+    fun providesHttpLoggingInterceptor() = HttpLoggingInterceptor()
+        .apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
+    @Singleton
+    @Provides
+    fun providesOkHttpClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient =
+        OkHttpClient
+            .Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(): Retrofit = Retrofit.Builder()
+        .addConverterFactory(GsonConverterFactory.create())
+        .baseUrl(BASE_URL)
+        .client(providesOkHttpClient(providesHttpLoggingInterceptor()))
+        .build()
+
+    @Singleton
+    @Provides
+    fun provideApiService(retrofit: Retrofit) : TeamApi =
+        retrofit.create(TeamApi::class.java)
+
+}
