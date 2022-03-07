f767ce24197d8af2f70b90f12f8cc9ef05ced171
minjun8946 <minjun8946@gmail.com>
2022-03-07 23:09:19 +0900

    ⚡️:: error handler module

--- SinpleArchitecture2/presentation/src/main/java/com/example/sinplearchitecture2/di/ErrorHandlerModule.kt
+++ SinpleArchitecture2/presentation/src/main/java/com/example/sinplearchitecture2/di/ErrorHandlerModule.kt
@@ -1,2 +1,19 @@
 package com.example.sinplearchitecture2.di
 
+import com.example.data.errorhandler.ErrorHandlerImpl
+import com.example.domain.ErrorHandler
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+
+abstract class ErrorHandlerModule {
+
+    @Binds
+    abstract fun provideErrorHandler(
+        errorHandlerImpl: ErrorHandlerImpl
+    ): ErrorHandler
+}
