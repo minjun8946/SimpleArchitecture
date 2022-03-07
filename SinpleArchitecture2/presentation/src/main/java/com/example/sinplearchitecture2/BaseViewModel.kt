1989374e6d5c45236e76e8adfcba0479105b976d
minjun8946 <minjun8946@gmail.com>
2022-03-07 23:09:02 +0900

    ⚡️:: base view model

--- SinpleArchitecture2/presentation/src/main/java/com/example/sinplearchitecture2/BaseViewModel.kt
+++ SinpleArchitecture2/presentation/src/main/java/com/example/sinplearchitecture2/BaseViewModel.kt
@@ -5,9 +5,17 @@ import androidx.lifecycle.viewModelScope
 import com.example.domain.base.UseCase
 import com.example.domain.usccase.TeamUseCase
 import kotlinx.coroutines.launch
+import java.lang.Exception
+import java.util.*
 
 open class BaseViewModel : ViewModel(){
 
-
+    suspend fun<T: UseCase,E> getApiResult(useCase: suspend () -> E):E {
+            return try {
+                useCase.invoke()
+            }catch (e: Exception){
+                throw e
+            }
+    }
 
 }
