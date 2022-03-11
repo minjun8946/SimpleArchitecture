53c506e37c9e205f9038987171184e5816b00e45
minjun8946 <minjun8946@gmail.com>
2022-03-11 10:23:36 +0900

    ⚡️:: team repository

--- SinpleArchitecture2/data/src/main/java/com/example/data/repository/TeamRepositoryImpl.kt
+++ SinpleArchitecture2/data/src/main/java/com/example/data/repository/TeamRepositoryImpl.kt
@@ -1,19 +1,28 @@
 package com.example.data.repository
 
-import com.example.data.remote.datasource.TeamDataSource
-import com.example.data.remote.dto.toEntity
+import com.example.data.local.datasource.LocalTeamDataSource
+import com.example.data.local.entity.toEntity
+import com.example.data.remote.datasource.RemoteTeamDataSource
+import com.example.data.util.CacheUtil
+import com.example.domain.base.CacheType
 import com.example.domain.entity.TeamEntity
 import com.example.domain.repository.TeamRepository
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class TeamRepositoryImpl @Inject constructor(
-    private val teamDataSource: TeamDataSource
+    private val remoteTeamDataSource: RemoteTeamDataSource,
+    private val localTeamDataSource: LocalTeamDataSource
 ) : TeamRepository {
-    override suspend fun getTeam(): Flow<TeamEntity> {
-        return flow {
-            emit(teamDataSource.getTeam().toEntity())
-        }
+
+    override suspend fun fetchTeam(): Flow<TeamEntity> =
+       CacheUtil<TeamEntity>()
+           .remoteDataSource { remoteTeamDataSource.fetchTeam() }
+           .localDataSource { localTeamDataSource.fetchTeam().toEntity() }
+           .insertRoom { localTeamDataSource.insertTeam(it) }
+           .cacheType(CacheType(CacheType.Type.ALWAYS))
+
+    override suspend fun delete(teamEntity: TeamEntity) {
+
     }
 }
