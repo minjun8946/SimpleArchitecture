aeb8489d3341823f1c1bef0df30d09e84804f0be
minjun8946 <minjun8946@gmail.com>
2022-03-11 10:25:17 +0900

    ⚡️:: team repository

--- SinpleArchitecture2/domain/src/main/java/com/example/domain/repository/TeamRepository.kt
+++ SinpleArchitecture2/domain/src/main/java/com/example/domain/repository/TeamRepository.kt
@@ -1,9 +1,12 @@
 package com.example.domain.repository
 
+import com.example.domain.base.CacheType
 import com.example.domain.entity.TeamEntity
 import kotlinx.coroutines.flow.Flow
 
 interface TeamRepository {
 
-    suspend fun getTeam(): Flow<TeamEntity>
+    suspend fun fetchTeam(): Flow<TeamEntity>
+
+    suspend fun delete(teamEntity: TeamEntity)
 }
