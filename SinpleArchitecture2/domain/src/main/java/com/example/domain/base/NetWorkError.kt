2b14ec1c80085e28ded2e78a9cd97fadd59f477e
minjun8946 <minjun8946@gmail.com>
2022-03-11 10:25:02 +0900

    ⚡️:: ent work error

--- SinpleArchitecture2/domain/src/main/java/com/example/domain/base/NetWorkError.kt
+++ SinpleArchitecture2/domain/src/main/java/com/example/domain/base/NetWorkError.kt
@@ -3,4 +3,4 @@ package com.example.domain.base
 import java.lang.RuntimeException
 
 class Unknown : RuntimeException()
-class NotFound(msg: String) : RuntimeException()
+class NotFound : RuntimeException()
