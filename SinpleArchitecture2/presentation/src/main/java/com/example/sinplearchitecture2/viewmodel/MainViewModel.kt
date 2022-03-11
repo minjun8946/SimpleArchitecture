d8069d3c89434011755d76fe14396f1914dce944
minjun8946 <minjun8946@gmail.com>
2022-03-11 10:27:25 +0900

    ⚡️:: test view model

--- SinpleArchitecture2/presentation/src/main/java/com/example/sinplearchitecture2/viewmodel/MainViewModel.kt
+++ SinpleArchitecture2/presentation/src/main/java/com/example/sinplearchitecture2/viewmodel/MainViewModel.kt
@@ -1,12 +1,10 @@
 package com.example.sinplearchitecture2.viewmodel
 
 import androidx.lifecycle.viewModelScope
-import com.example.data.errorhandler.ErrorHandlerImpl
 import com.example.domain.base.NotFound
 import com.example.domain.usccase.TeamUseCase
 import com.example.sinplearchitecture2.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -28,5 +26,4 @@ class MainViewModel @Inject constructor(
             }
         }
     }
-
 }
